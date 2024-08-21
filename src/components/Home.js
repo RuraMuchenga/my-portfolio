5c5add0362bad5d932a8dc60cefc24edce8726d9
Ruramai <ruramaimuchenga@gmail.com>
2024-08-21 22:41:23 +0200

    latest version

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,7 +14,7 @@ function Home() {
         <section id="home">
             <div className="intro-text">
                 <h2>Hello there! I'm Ruramai Muchenga👋.</h2>
-                <p>I am a junior Software Developer residing in Dubai, UAE. Currently in my final year at Middlesex University Dubai, I am passionate about crafting software solutions that address real-world challenges. When I'm not immersed in the world of code, I am watching TV shows such as Suits and The Blacklist, appreciating the intricate plots and character developments. I also enjoy working out and going to the gym (i am a bit of an addict , i work out 5 times a week for at least an hour and 30 min each.) In addition to my technological pursuits, I am honing my skills in chess, aspiring to play the game more professionally.</p>
+                <p>I am a junior Software Developer based in Dubai, UAE. As I approach the conclusion of my studies at Middlesex University Dubai, my enthusiasm for developing software solutions that tackle real-world issues continues to grow even more. Outside of my programming skills, I enjoy watching TV series like Suits and The Blacklist, captivated by their complex storylines and character arcs. Fitness is also a major part of my life; I’m dedicated to working out five times a week for at least an hour and a half each session. Additionally, I’m refining my chess skills with hopes of advancing to a more professional level.</p>
             </div>
             <div className="profile-pic-container">
                 <img src={profilePic} alt="Profile" />
