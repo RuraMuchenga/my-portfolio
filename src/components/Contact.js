1c01809ea925fd13d3d0f21f6b888e0af8bbb9f8
Ruramai <ruramaimuchenga@gmail.com>
2024-10-25 15:56:57 +0400

    latest version

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,3 +1,64 @@
+// import React from 'react';
+// import './Contact.css'; 
+// import twitterIcon from './images/twitter-icon.png';
+// import githubIcon from './images/github-icon.png';
+// import linkedinIcon from './images/linkedin-icon.png';
+// import emailIcon from './images/email-icon.png';
+
+// const Contact = () => {
+//     return (
+//         <div id="contact">
+//             <h2>Lets chat!</h2>
+//             <form>
+//                 <div>
+//                     <input type="text" id="name" name="name" placeholder=" " required />
+//                     <label htmlFor="name">Name</label>
+//                 </div>
+//                 <div>
+//                     <input type="email" id="email" name="email" placeholder=" " required />
+//                     <label htmlFor="email">Email</label>
+//                 </div>
+//                 <div>
+//                     <textarea id="message" name="message" placeholder=" " required></textarea>
+//                     <label htmlFor="message">Message</label>
+//                 </div>
+//                 <button type="submit">Send Message</button>
+//             </form>
+
+//             <div id="social-links">
+//                 <a href="https://x.com/_ruraaa" target="_blank" rel="noopener noreferrer">
+//                     <img src={twitterIcon} alt="Twitter" /> Twitter
+//                 </a>
+//                 <a href="https://github.com/RuraMuchenga" target="_blank" rel="noopener noreferrer">
+//                     <img src={githubIcon} alt="GitHub" /> GitHub
+//                 </a>
+//                 <a href="https://www.linkedin.com/in/ruramai-muchenga-a20454165/" target="_blank" rel="noopener noreferrer">
+//                     <img src={linkedinIcon} alt="LinkedIn" /> LinkedIn
+//                 </a>
+//                 <a href="ruramaimuchenga@gmail.com">
+//                     <img src={emailIcon} alt="Email" /> Email
+//                 </a>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default Contact;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import React from 'react';
 import './Contact.css'; 
 import twitterIcon from './images/twitter-icon.png';
@@ -8,22 +69,8 @@ import emailIcon from './images/email-icon.png';
 const Contact = () => {
     return (
         <div id="contact">
-            <h2>Lets chat!</h2>
-            <form>
-                <div>
-                    <input type="text" id="name" name="name" placeholder=" " required />
-                    <label htmlFor="name">Name</label>
-                </div>
-                <div>
-                    <input type="email" id="email" name="email" placeholder=" " required />
-                    <label htmlFor="email">Email</label>
-                </div>
-                <div>
-                    <textarea id="message" name="message" placeholder=" " required></textarea>
-                    <label htmlFor="message">Message</label>
-                </div>
-                <button type="submit">Send Message</button>
-            </form>
+            <h2>Let's Talk</h2>
+            <p>If you are interested in working with me. Feel free to reach out to me.</p>
 
             <div id="social-links">
                 <a href="https://x.com/_ruraaa" target="_blank" rel="noopener noreferrer">
@@ -44,4 +91,3 @@ const Contact = () => {
 };
 
 export default Contact;
-
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,3 +1,89 @@
+// import React from 'react';
+// import './Resume.css';
+
+// const resumePDF = 'https://drive.google.com/file/d/1CpsFCJIuVrHJFWBU3gBMYUgkasVvM5kR/view?usp=sharing'; 
+
+// const Resume = () => {
+//     return (
+//         <section id="resume">
+//             <h2>Resume</h2>
+//             <div className="resume-content">
+//                 <a href={resumePDF} target="_blank" rel="noopener noreferrer" className="resume-link">
+//                     View My Resume
+//                 </a>
+//                 <div className="education">
+//                     <h3>Education</h3>
+//                     <ul>
+//                         <li>Bachelor's Degree in Computer Systems Engineering, Middlesex University Dubai (Expected June 2025)</li>
+//                         <li>A-Levels</li>
+//                         <li>IGCSE O-Levels</li>
+//                     </ul>
+//                 </div>
+//                 <div className="skills">
+//                     <h3>Skills</h3>
+//                     <ul>
+//                         <li>Web Development (HTML, CSS5, JavaScript, React)</li>
+//                         <li>Software Development</li>
+//                         <li>Problem-Solving and Analytical Skills</li>
+//                         <li>Project Management</li>
+//                         <li>Freelance work</li>
+//                     </ul>
+//                 </div>
+//                 <div className="projects">
+//                     <h3>Projects</h3>
+//                     <ul>
+//                         <li>Personal Portfolio Website</li>
+//                         <li>AirPiano Project</li>
+//                         <li>iFit Studio Project</li>
+//                         <li>Customer Churn Prediction</li>
+//                         <li>Home Security system project</li>
+//                         <li>Sentiment Analysis Tool Project</li>
+//                         <li>Used Matlab to simulate a communication system which
+// takes an input sequence of bits entered from the keyboard
+// performs vector encoding using 128QAM constellation.</li>
+//                         <li>Open-Source Contributions</li>
+//                     </ul>
+//                 </div>
+//             </div>
+//         </section>
+//     );
+// };
+
+// export default Resume;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import React from 'react';
 import './Resume.css';
 
@@ -8,41 +94,34 @@ const Resume = () => {
         <section id="resume">
             <h2>Resume</h2>
             <div className="resume-content">
-                <a href={resumePDF} target="_blank" rel="noopener noreferrer" className="resume-link">
-                    View My Resume
-                </a>
-                <div className="education">
-                    <h3>Education</h3>
-                    <ul>
-                        <li>Bachelor's Degree in Computer Systems Engineering, Middlesex University Dubai (Expected June 2025)</li>
-                        <li>A-Levels</li>
-                        <li>IGCSE O-Levels</li>
-                    </ul>
+                <div className="resume-header">
+                    <a href={resumePDF} target="_blank" rel="noopener noreferrer" className="download-button">
+                        Download <span>↓</span>
+                    </a>
                 </div>
-                <div className="skills">
-                    <h3>Skills</h3>
-                    <ul>
-                        <li>Web Development (HTML, CSS5, JavaScript, React)</li>
-                        <li>Software Development</li>
-                        <li>Problem-Solving and Analytical Skills</li>
-                        <li>Project Management</li>
-                        <li>Freelance work</li>
-                    </ul>
-                </div>
-                <div className="projects">
-                    <h3>Projects</h3>
-                    <ul>
-                        <li>Personal Portfolio Website</li>
-                        <li>AirPiano Project</li>
-                        <li>iFit Studio Project</li>
-                        <li>Customer Churn Prediction</li>
-                        <li>Home Security system project</li>
-                        <li>Sentiment Analysis Tool Project</li>
-                        <li>Used Matlab to simulate a communication system which
-takes an input sequence of bits entered from the keyboard
-performs vector encoding using 128QAM constellation.</li>
-                        <li>Open-Source Contributions</li>
-                    </ul>
+                <div className="experience">
+                    <div className="job">
+                        <div className="job-details">
+                            <h4>Student learning assistant</h4>
+                            <span className="job-location">Harare, Zimbabwe</span>
+                        </div>
+                        <p>Supported professors and lecturers by clarifying difficult concepts during lectures or
+                        tutorials.</p>
+                    </div>
+                    <div className="job">
+                        <div className="job-details">
+                            <h4>Junior Fullstack Developer</h4>
+                            <span className="job-location">Dubai, United Arab Emirates</span>
+                        </div>
+                        <p>Freelance developer for various small-scale projects, particularly working with flutter,react, java, css and machine learning.</p>
+                    </div>
+                    <div className="job">
+                        <div className="job-details">
+                            <h4>Backend Developer</h4>
+                            <span className="job-location">Dubai, United Arab Emirates</span>
+                        </div>
+                        <p>Freelance developer for various small-scale backend projects, optimizing databases and working on API integration.</p>
+                    </div>
                 </div>
             </div>
         </section>
@@ -50,4 +129,3 @@ performs vector encoding using 128QAM constellation.</li>
 };
 
 export default Resume;
-
